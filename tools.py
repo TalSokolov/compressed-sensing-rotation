6129a78126fb8032e1e7ae2f481a4f894e9001fb
talsokolov <talsokolov@gmail.com>
2022-05-29 13:41:17 +0300

    print f1 score

--- tools.py
+++ tools.py
@@ -27,10 +27,12 @@ def evaluate(ch, gt, idx, run_name):
     TP = np.sum((bool_gt + bool_ch) == 2)/(area)
     TN = np.sum((bool_gt + bool_ch) == 0)/(area)
 
+
     plt.figure(idx)
     plt.pie([FP, FN, TP, TN], labels=['False Positive', 'False Negative', 'True Positive', 'True Negative'], autopct='%1.1f%%')
     plt.title('Channel {}'.format(CHANNELS[idx]))
     plt.savefig(os.path.join(save_path, 'eval {}_{}.tiff'.format(CHANNELS[idx], run_name)))
+    print('{} f1 score: {}'.format(CHANNELS[idx], str(TP/(TP+0.5(FP+FN)))))
     plt.close()
     return
 
